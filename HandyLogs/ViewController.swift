60a1039e29f653629f3744fb952ecb886fa58512
giftbott <giftbott@gmail.com>
2017-04-30 20:34:27 +0900

    Create Xcode Project

---
+++ HandyLogs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandyLogs
+//
+//  Created by giftbot on 2017. 4. 30..
+//  Copyright © 2017년 giftbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
