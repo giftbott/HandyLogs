b700f77d938f5b0e11408c8deced001f0c47c760
giftbott <giftbott@gmail.com>
2017-04-30 22:26:26 +0900

    Add Demo Code

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -12,14 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        Handy.log("Default log type is info type")
+        Handy.log("You can change log type easily", logType: .warning)
+        Handy.log("Variadic parameters are available\n",
+                  type(of: self), "'s frame :", self.view.frame, ":D", logType: .debug)
+        
+        Handy.defaultLogType = .debug
+        Handy.LogType.debugEmoji = "🔰"
+        Handy.LogType.infoEmoji = "🔥"
+        Handy.log("DefaultLogType & emoji are changable")
+                
+        Handy.log("If you set custom flag: SUBDIVIDE", logType: .fatal)
+        
+        #if SUBDIVIDE
+            Handy.cLog("Subdivided functions will be provided")
+            Handy.dLog("to more convenience use for each log type")
+            Handy.wLog()
+            Handy.eLog()
+            Handy.fLog()
+        #endif
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
